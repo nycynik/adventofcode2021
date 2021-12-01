5a71d2ab1aa39ff1feb4e853451061df9c1040e4
Mike Lynch <nycynik@gmail.com>
2021-12-01 08:43:06 -0800

    day 01

--- src/day01/part1.ts
+++ src/day01/part1.ts
@@ -1,5 +1,22 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input: string): number {
-  return 0;
+
+  let count = 0
+
+  const splitInput = input.split(/[\r\n]+/)
+  if (splitInput.length == 0) return 0;
+
+  for (let i = 1; i < splitInput.length; i++) {
+    const value = parseInt(splitInput[i])
+    const lastValue = parseInt(splitInput[i-1])
+    if (value > lastValue) {
+      count += 1;
+      //console.log(`${value} vs ${lastValue} (increased ++)`)
+    } else {
+      //console.log(`${value} vs ${lastValue} (decreased --)`)
+    }
+  }
+  
+  return count;
 }
--- src/day01/part2.ts
+++ src/day01/part2.ts
@@ -1,5 +1,24 @@
 // Advent of Code - Day 1 - Part Two
+// sliding window is not real, since hte math is simply 
+// addition, we can ignore the common factors in comparison
 
 export function part2(input: string): number {
-  return 0;
+  let count = 0
+
+  const splitInput = input.split(/[\r\n]+/)
+  if (splitInput.length < 4) return 0;
+
+  for (let i = 3; i < splitInput.length; i++) {
+    const value = parseInt(splitInput[i])
+    const lastValue = parseInt(splitInput[i-3])
+
+    if (value > lastValue) {
+      count += 1;
+      //console.log(`${value} vs ${lastValue} (increased ++)`)
+    } else {
+      //console.log(`${value} vs ${lastValue} (decreased --)`)
+    }
+  }
+  
+  return count;
 }
