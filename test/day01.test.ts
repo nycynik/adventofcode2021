bd0a005add033002a9e7dc2038aa478dc5b5e2e7
Mike Lynch <nycynik@gmail.com>
2021-12-01 12:00:27 -0800

    tests for day 1

--- test/day01.test.ts
+++ test/day01.test.ts
@@ -3,9 +3,27 @@
 import { part1, part2 } from '../src/day01';
 
 test('part one test', () => {
-  expect(part1("")).toBe(0);
+  expect(part1(`199
+  200
+  208
+  210
+  200
+  207
+  240
+  269
+  260
+  263`)).toBe(7);
 });
 
 test('part two test', () => {
-  expect(part2("")).toBe(0);
+  expect(part2(`199
+  200
+  208
+  210
+  200
+  207
+  240
+  269
+  260
+  263`)).toBe(5);
 });
